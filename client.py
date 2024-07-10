4977092379311a3e637bf9ee41124d24cb3e7a99
Georgi <georgi.manev@entra.energy>
2024-07-10 17:53:41 +0300

    up

--- client.py
+++ client.py
@@ -106,7 +106,7 @@ async def main():
     
      
 if __name__ == "__main__":
-    asyncio.run(main())
-    # loop = asyncio.get_event_loop()
-    # loop.create_task(main())
-    # loop.run_forever()
+    #asyncio.run(main())
+    loop = asyncio.get_event_loop()
+    loop.create_task(main())
+    loop.run_forever()
