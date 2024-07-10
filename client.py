fc7a75d5ac9bda7693b6c2450e5982dca5ccd6da
Georgi <georgi.manev@entra.energy>
2024-07-10 17:58:43 +0300

    up

--- client.py
+++ client.py
@@ -16,31 +16,30 @@ class DataPublisher:
         self.topic_wind = topic_wind
         self.topic_power = topic_power
 
-    async def publish_data(self):        
+    async def publish_data(self):
         while True:
             try:
                 wind_value, power_value = await self.opcua_client.read_data()                
                 print(f'Wind Speed: {wind_value.Value.Value} m/s')
-                url_wind = f"https://fra1.blynk.cloud/external/api/batch/update?token=RDng9bL06n9TotZY9sNvssAYxIoFPik8&v5={wind_value.Value.Value}" # Aris wind
-                #url_wind = f"https://fra1.blynk.cloud/external/api/batch/update?token=RDng9bL06n9TotZY9sNvssAYxIoFPik8&v11={wind_value.Value.Value}" # Neykovo wind
+                url_wind = f"https://fra1.blynk.cloud/external/api/batch/update?token=RDng9bL06n9TotZY9sNvssAYxIoFPik8&v5={wind_value.Value.Value}" # Aris
+                #url_wind = f"https://fra1.blynk.cloud/external/api/batch/update?token=RDng9bL06n9TotZY9sNvssAYxIoFPik8&v11={wind_value.Value.Value}" # Power
                     
                 r_wind = requests.get(url_wind)
                 if r_wind.status_code == 200:
                     pass
                 print(f'Power: {power_value.Value.Value} kW')
-                url_power = f"https://fra1.blynk.cloud/external/api/batch/update?token=RDng9bL06n9TotZY9sNvssAYxIoFPik8&v4={power_value.Value.Value}"  # Aris power
-                #url_power = f"https://fra1.blynk.cloud/external/api/batch/update?token=RDng9bL06n9TotZY9sNvssAYxIoFPik8&v10={power_value.Value.Value}" # Neykovo power
+                url_power = f"https://fra1.blynk.cloud/external/api/batch/update?token=RDng9bL06n9TotZY9sNvssAYxIoFPik8&v4={power_value.Value.Value}"  # Aris
+                #url_power = f"https://fra1.blynk.cloud/external/api/batch/update?token=RDng9bL06n9TotZY9sNvssAYxIoFPik8&v10={power_value.Value.Value}" # Power
                 r_power = requests.get(url_power) 
                 if r_power.status_code == 200:
                     pass
-                # await self.mqtt_client.connect_and_publish(self.topic_wind, str(round(wind_value.Value.Value, 2)))
-                # await self.mqtt_client.connect_and_publish(self.topic_power, str(round(power_value.Value.Value, 2)))
+                await self.mqtt_client.connect_and_publish(self.topic_wind, str(round(wind_value.Value.Value, 2)))
+                await self.mqtt_client.connect_and_publish(self.topic_power, str(round(power_value.Value.Value, 2)))
             except ua.UaStatusCodeError as e:
                 print(f"OPC UA Error: {e}")
             except Exception as e:
                 print(f"Unexpected error: {e}")
-            await asyncio.sleep(60) 
-             
+            await asyncio.sleep(30)   
 
 class TourbineControl:
     def __init__(self, file_manager, opcua_client) -> None:
@@ -48,11 +47,9 @@ class TourbineControl:
         self.opcua_client = opcua_client
     
     async def scheduler_check(self):
-        print("HERE!!!!")
         next_forecast_value = await self.file_manager.process_files()
         current_status = await self.status_check()
-        print(current_status)
-        # print(f"Turbine Current Status: {current_status} || command:{next_forecast_value}")        
+        print(f"Turbine Current Status: {current_status} || command:{next_forecast_value}")        
         if next_forecast_value:            
             url = "https://fra1.blynk.cloud/external/api/batch/update?token=RDng9bL06n9TotZY9sNvssAYxIoFPik8&v0=1"
             r = requests.get(url)
@@ -70,15 +67,13 @@ async def main():
     cert_base = Path(__file__).parent    
     
     url_aris = "opc.tcp://10.126.252.1:62550/DataAccessServer"
+    url_power = "opc.tcp://10.126.253.1:62550/DataAccessServer"
     wind_node_aris = 'ns=2;s=DA.Rakovo Aris.WTG01.WMET01.HorWdSpd'
+    wind_node_neykovo = 'ns=2;s=DA.Neykovo.WTG01.WMET01.HorWdSpd'
     power_node_aris = 'ns=2;s=DA.Rakovo Aris.WTG01.WTUR01.W'
+    power_node_neykovo = 'ns=2;s=DA.Neykovo.WTG01.WTUR01.W'
     status_node_aris = 'ns=2;s=DA.Rakovo Aris.WTG01.WTUR01.TurSt'
-
-    # url_power = "opc.tcp://10.126.253.1:62550/DataAccessServer"    
-    # wind_node_neykovo = 'ns=2;s=DA.Neykovo.WTG01.WMET01.HorWdSpd'    
-    # power_node_neykovo = 'ns=2;s=DA.Neykovo.WTG01.WTUR01.W'    
-    # status_node_neykovo = 'ns=2;s=DA.Neykovo.WTG01.WTUR01.TurSt'
-
+    status_node_neykovo = 'ns=2;s=DA.Neykovo.WTG01.WTUR01.TurSt'
     opcua_client = OPCUAClient(        
         url = url_aris, #url_power        
         client_app_uri="urn:freeopcua:client",
@@ -90,19 +85,18 @@ async def main():
     )
     await opcua_client.setup()
     process_the_file = FileManager()
-    turbine_control = TourbineControl(process_the_file, opcua_client)  
+    tourbine_control = TourbineControl(process_the_file, opcua_client)
+    scheduler = AsyncIOScheduler()    
+    scheduler.add_job(tourbine_control.scheduler_check, IntervalTrigger(minutes=1))
+    scheduler.start()
     # Start/Stop the turine    
     #await opcua_client.send_stop_start_command("start")
+
     mqtt_client = MQTTClient(broker="159.89.103.242", port=1883)    
     publisher = DataPublisher(opcua_client, mqtt_client, topic_wind='power/1mwind', topic_power='power/1mpow')#power/aris
-    await publisher.publish_data()
-    scheduler = AsyncIOScheduler()
-    scheduler.add_job(turbine_control.scheduler_check, IntervalTrigger(minutes=1))
-    
-    scheduler.start()
 
-    # Run forever
-    await asyncio.Event().wait()
+    await publisher.publish_data()
+    # Start the scheduler
     
      
 if __name__ == "__main__":
