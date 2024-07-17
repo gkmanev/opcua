ed159d57d34a326e8ab28f2937bb54ca5960e298
Georgi <georgi.manev@entra.energy>
2024-07-17 16:31:36 +0300

    up

--- opcua_setup.py
+++ opcua_setup.py
@@ -37,9 +37,10 @@ class OPCUAClient:
                 
                 get_status_node = self.client.get_node(self.status_node)
                 status_value = await get_status_node.read_data_value()
+                
 
-                stop_node = self.client.get_node('ns=2;s=DA.Rakovo Aris.WTG01.WTUR01.TurStrOp')
-                await stop_node.set_value(ua.Variant(True, ua.VariantType.Boolean))
+                # stop_node = self.client.get_node('ns=2;s=DA.Rakovo Aris.WTG01.WTUR01.TurStrOp')
+                # await stop_node.set_value(ua.Variant(True, ua.VariantType.Boolean))
                
 
 
@@ -51,15 +52,21 @@ class OPCUAClient:
         except Exception as e:
             _logger.error(f"An unexpected error occurred: {e}")
         
-    # async def send_stop_start_command(self, command):
-            
-        
-    #     stop_node = self.client.get_node('ns=2;s=DA.Rakovo Aris.WTG01.WTUR01.TurStopOp')
-    #     print(f"SSSSSSSSSSSSSSSSSSSTTTTTTTTTTTOP:{stop_node}")
-    #     await stop_node.set_value(ua.Variant(True, ua.VariantType.Boolean))
-    #         # await stop_node.set_value(ua.Variant(True, ua.VariantType.Boolean))
-    #         # #Start
-    #         # start_node = self.client.get_node(self.start_node)
+    async def send_stop_start_command(self, command):   
+        try:
+            async with self.client:       
+                stop_node = self.client.get_node('ns=2;s=DA.Rakovo Aris.WTG01.WTUR01.TurStopOp')
+                print(f"SSSSSSSSSSSSSSSSSSSTTTTTTTTTTTOP:{stop_node}")
+                await stop_node.set_value(ua.Variant(True, ua.VariantType.Boolean))
+            # await stop_node.set_value(ua.Variant(True, ua.VariantType.Boolean))
+            # #Start
+            # start_node = self.client.get_node(self.start_node)
+        except asyncua.ua.UaStatusCodeError as e:
+            _logger.error(f"OPC UA status code error: {e}")
+        except asyncio.CancelledError:
+            _logger.error("Task was cancelled")
+        except Exception as e:
+            _logger.error(f"An unexpected error occurred: {e}")
               
 
             
