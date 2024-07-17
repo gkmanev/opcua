b1db984dc3c9c3e36b91b783c8563839abdb79a8
Georgi <georgi.manev@entra.energy>
2024-07-17 14:47:13 +0300

    up

--- client_neykovo.py
+++ client_neykovo.py
@@ -97,7 +97,7 @@ class DataPublisher:
         current_time_minute = datetime.now().minute 
         if self.next_forecast_value:
             if self.next_forecast_value != "NA":
-                if self.turbine_status == 1:
+                if self.turbine_status == 2:
                     #if current_time_minute % 15 == 0:                    
                     await self.opcua_client.send_stop_start_command("start")                
             else:
