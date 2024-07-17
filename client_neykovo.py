02fdf71ac8290270b2eeef082291ebe92e75bd9c
Georgi <georgi.manev@entra.energy>
2024-07-17 12:38:51 +0300

    up

--- client_neykovo.py
+++ client_neykovo.py
@@ -1,4 +1,5 @@
 import asyncio
+import aiohttp
 from pathlib import Path
 from opcua_setup import OPCUAClient
 from mqtt import MQTTClient
@@ -19,6 +20,7 @@ class DataPublisher:
         self.email_processor = email_processor
         self.turbine_status = None      
         self.accumulate_power = 0 
+        self.next_forecast_value = None
 
 
 
@@ -28,47 +30,79 @@ class DataPublisher:
             self.turbine_status = turbine_status.Value.Value  
             print(f'Wind Speed: {wind_value.Value.Value} m/s')
             url_wind = f"https://fra1.blynk.cloud/external/api/batch/update?token=RDng9bL06n9TotZY9sNvssAYxIoFPik8&v11={wind_value.Value.Value}" # Neykovo
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url_wind) as response:
+                    if response.status == 200:
+                        pass 
                 
-            r_wind = requests.get(url_wind)
-            if r_wind.status_code == 200:
-                pass
+            
             current_minute = datetime.now().minute
             if current_minute % 15 == 0:
                 self.accumulate_power = 0
                 print("Accumulate resetting on every 15th min")
             self.accumulate_power += int(power_value.Value.Value)
             print(f"accumulated energy = {self.accumulate_power}")
+
             url_neykovo_accumulate = f"https://fra1.blynk.cloud/external/api/batch/update?token=RDng9bL06n9TotZY9sNvssAYxIoFPik8&v8={self.accumulate_power/60}"  # Neykovo  
-            r_accumulate = requests.get(url_neykovo_accumulate)
-            if r_accumulate.status_code == 200:
-                pass
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url_neykovo_accumulate) as response:
+                    if response.status == 200:
+                        pass 
             print(f'Power: {power_value.Value.Value} kW')
+
             url_power = f"https://fra1.blynk.cloud/external/api/batch/update?token=RDng9bL06n9TotZY9sNvssAYxIoFPik8&v10={power_value.Value.Value}" # Neykovo
-            r_power = requests.get(url_power) 
-            if r_power.status_code == 200:
-                pass            
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url_power) as response:
+                    if response.status == 200:
+                        pass  
+
+            #get_and_publish_the_forecast_data
+            self.next_forecast_value = await self.email_processor.process_files()
+            value_published_to_blynk = None
+            if self.next_forecast_value:
+                if self.next_forecast_value == "NA":
+                    value_published_to_blynk = 0
+                else:
+                    value_published_to_blynk = self.next_forecast_value          
+                url_forecast = f"https://fra1.blynk.cloud/external/api/batch/update?token=RDng9bL06n9TotZY9sNvssAYxIoFPik8&v9={value_published_to_blynk}" #V9 Neykovo V2 Aris
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(url_forecast) as response:
+                        if response.status == 200:
+                            pass    
+            #publish turbine status
+            if self.turbine_status == 3:  
+                url = "https://fra1.blynk.cloud/external/api/batch/update?token=RDng9bL06n9TotZY9sNvssAYxIoFPik8&v17=1"
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(url) as response:
+                        if response.status == 200:
+                            pass 
+            if self.turbine_status == 1:
+                url = "https://fra1.blynk.cloud/external/api/batch/update?token=RDng9bL06n9TotZY9sNvssAYxIoFPik8&v17=0"
+                async with aiohttp.ClientSession() as session:
+                        async with session.get(url) as response:
+                            if response.status == 200:
+                                pass           
+
+
+
         except ua.UaStatusCodeError as e:
             print(f"OPC UA Error: {e}")
         except Exception as e:
             print(f"Unexpected error: {e}")
-    
-    async def turbine_control(self):
-        next_forecast_value = await self.email_processor.process_files()        
-        print(f"Turbine Current Status: {self.turbine_status} || command:{next_forecast_value}") 
-        if next_forecast_value:
-            if next_forecast_value != "NA":      
-                converted_to_kw = float(next_forecast_value)*1000
-                url_forecast = f"https://fra1.blynk.cloud/external/api/batch/update?token=RDng9bL06n9TotZY9sNvssAYxIoFPik8&v9={converted_to_kw}" #V9 Neykovo V2 Aris
-                r_forecast = requests.get(url_forecast)
-                if r_forecast.status_code == 200:
-                    pass
-                url = "https://fra1.blynk.cloud/external/api/batch/update?token=RDng9bL06n9TotZY9sNvssAYxIoFPik8&v17=1"
-                r = requests.get(url)
-                if r.status_code == 200:
-                    pass
+
+        
+    async def turbine_control(self):                
+        # print(f"Turbine Current Status: {self.turbine_status} || command:{next_forecast_value}")
+        current_time_minute = datetime.now().minute 
+        if self.next_forecast_value:
+            if self.next_forecast_value != "NA":
+                if self.turbine_status == 1:
+                    if current_time_minute % 15 == 0:                    
+                        await self.opcua_client.send_stop_start_command("start")                
             else:
-                url = "https://fra1.blynk.cloud/external/api/batch/update?token=RDng9bL06n9TotZY9sNvssAYxIoFPik8&v17=0"
-                r = requests.get(url)               
+                if self.turbine_status == 3:
+                    if current_time_minute % 15 == 0:
+                        await self.opcua_client.send_stop_start_command("stop")
 
 
 async def main():
@@ -78,6 +112,9 @@ async def main():
     wind_node_neykovo = 'ns=2;s=DA.Neykovo.WTG01.WMET01.HorWdSpd'    
     power_node_neykovo = 'ns=2;s=DA.Neykovo.WTG01.WTUR01.W'    
     status_node_neykovo = 'ns=2;s=DA.Neykovo.WTG01.WTUR01.TurSt'
+    #start/stop
+    start_node_neykovo = 'ns=2;s=DA.Neykovo.WTG01.WTUR01.TurStrOp'
+    stop_node_neykovo = 'ns=2;s=DA.Neykovo.WTG01.WTUR01.TurStopOp'
     opcua_client = OPCUAClient(        
         url = url_power,     
         client_app_uri="urn:freeopcua:client",
@@ -85,7 +122,9 @@ async def main():
         private_key_path=cert_base / "my_private_key.pem",
         wind_node = wind_node_neykovo,
         power_node = power_node_neykovo,
-        status_node = status_node_neykovo
+        status_node = status_node_neykovo,
+        start_turbine = start_node_neykovo,
+        stop_turbine = stop_node_neykovo
     )
     await opcua_client.setup()
     email_forecast_processor = FileManager("neykovo") 
@@ -96,7 +135,7 @@ async def main():
 
     scheduler.add_job(publisher.publish_data, IntervalTrigger(minutes=1))
     scheduler.add_job(publisher.turbine_control, IntervalTrigger(minutes=1))
-    
+
     scheduler.add_job(gmail_processor.proceed_forecast, CronTrigger(hour=10, minute=15))
     scheduler.add_job(gmail_processor.proceed_forecast, CronTrigger(hour=11, minute=15))
     scheduler.add_job(gmail_processor.proceed_forecast, CronTrigger(hour=12, minute=15))  
--- opcua_setup.py
+++ opcua_setup.py
@@ -9,14 +9,16 @@ _logger = logging.getLogger('asyncua')
 
 
 class OPCUAClient:
-    def __init__(self, url, client_app_uri, cert_path, private_key_path, wind_node, power_node, status_node):
+    def __init__(self, url, client_app_uri, cert_path, private_key_path, wind_node, power_node, status_node, start_node, stop_node):
         self.client = UA_Client(url=url)
         self.client.application_uri = client_app_uri
         self.cert_path = cert_path
         self.private_key_path = private_key_path
         self.wind_node = wind_node
         self.power_node = power_node
-        self.status_node = status_node
+        self.status_node = status_node,
+        self.start_node = start_node,
+        self.stop_node = stop_node
 
     async def setup(self):
         await self.client.set_security_string(f"Basic256,SignAndEncrypt,{self.cert_path},{self.private_key_path}")
@@ -43,22 +45,19 @@ class OPCUAClient:
         except Exception as e:
             _logger.error(f"An unexpected error occurred: {e}")
         
-    async def send_stop_start_command(self,command):
-        print("Write to OPC")
-        async with self.client:
-            pass            
-            #Stop
-            # stop_node = self.client.get_node('ns=2;s=DA.Rakovo Aris.WTG01.WTUR01.TurStopOp')
+    async def send_stop_start_command(self, command):
+            try:
+            # #Stop
+            # stop_node = self.client.get_node(self.stop_node)
             # await stop_node.set_value(ua.Variant(True, ua.VariantType.Boolean))
-            #Start
-            #start_node = self.client.get_node('ns=2;s=DA.Rakovo Aris.WTG01.WTUR01.TurStrOp')
-            # tag_node = ''
-            # if command == 'start':
-            #     tag_node = 'ns=2;s=DA.Rakovo Aris.WTG01.WTUR01.TurStrOp'
-            # elif command == 'stop':
-            #     tag_node = 'ns=2;s=DA.Rakovo Aris.WTG01.WTUR01.TurStopOp'
-            # command_node = self.client.get_node(tag_node)
-            # await command_node.set_value(ua.Variant(True, ua.VariantType.Boolean))           
+            # #Start
+            # start_node = self.client.get_node(self.start_node)
+                tag_node = self.start_node if command == 'start' else self.stop_node
+                command_node = self.client.get_node(tag_node)
+                _logger.info(f"{command}ing the turbine...")
+                await command_node.set_value(ua.Variant(True, ua.VariantType.Boolean))     
+            except Exception as e:
+                _logger.error(f"Failed to {command} the turbine: {e}")  
  
     async def check_tourbine_status(self):
         async with self.client:          
