0e62eaed242afa7837f0bb5068b47faf0f23fb61
Georgi <georgi.manev@entra.energy>
2025-04-03 16:48:13 +0300

    up

--- client_neykovo.py
+++ client_neykovo.py
@@ -162,7 +162,7 @@ async def main():
     scheduler.add_job(gmail_processor.proceed_forecast, CronTrigger(hour=12, minute=15))  
 
     scheduler.add_job(partial(gmail_processor.proceed_forecast, clearing=True), CronTrigger(hour=15, minute=0))
-    scheduler.add_job(partial(gmail_processor.proceed_forecast, clearing=True), CronTrigger(hour=16, minute=43))
+    scheduler.add_job(partial(gmail_processor.proceed_forecast, clearing=True), CronTrigger(hour=16, minute=50))
     scheduler.add_job(partial(gmail_processor.proceed_forecast, clearing=True), CronTrigger(hour=17, minute=0))
 
 
