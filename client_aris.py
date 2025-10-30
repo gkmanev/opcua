c16d012376b4c2907e51568fffae3877cab02b82
Georgi <georgi.manev@entra.energy>
2025-10-30 11:35:02 +0200

    update_after_revert1

--- client_aris.py
+++ client_aris.py
@@ -252,7 +252,7 @@ async def main():
     scheduler.add_job(publisher.publish_data, IntervalTrigger(minutes=1))
     #scheduler.add_job(publisher.turbine_control, IntervalTrigger(minutes=1))  
     scheduler.add_job(gmail_processor.proceed_forecast, CronTrigger(hour=10, minute=10))
-    scheduler.add_job(gmail_processor.proceed_forecast, CronTrigger(hour=11, minute=31))
+    scheduler.add_job(gmail_processor.proceed_forecast, CronTrigger(hour=11, minute=36))
     scheduler.add_job(gmail_processor.proceed_forecast, CronTrigger(hour=15, minute=15))  
 
     scheduler.add_job(partial(gmail_processor.proceed_forecast, clearing=True), CronTrigger(hour=15, minute=0))
